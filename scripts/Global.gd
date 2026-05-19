9b7f6324f7d321cd867093e25e9736c025486ca9
Funj13 <caiquegames67@gmail.com>
2026-05-19 11:49:36 -0300

    docs: Recomeco do Projeto na UE5.

--- scripts/Global.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-func hit_stop(time_scale_novo: float, duracao: float):
-	Engine.time_scale = time_scale_novo
-	# Espera o tempo (usando o timer da árvore, ignorando o time_scale atual)
-	await get_tree().create_timer(duracao * time_scale_novo, true, false, true).timeout
-	Engine.time_scale = 1.0
--- scripts/Interactable.gd
+++
@@ -1,8 +0,0 @@
-extends StaticBody3D
-class_name Interagivel
-
-# Todo objeto interativo vai ter essa função.
-# O Baú vai sobrescrever isso para abrir.
-# O NPC vai sobrescrever isso para falar.
-func interagir(_player):
-	print("Interagiu com algo genérico.")
